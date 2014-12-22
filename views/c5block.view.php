cbe8e4f8c07bd5ede1e9fbf7deaa01323d47c6fa
Fumito Mizuno <ounziw@gmail.com>
2014-12-22 11:20:31 +0900

    javascript footer

--- views/c5block.view.php
+++ views/c5block.view.php
@@ -6,7 +6,12 @@
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
  * @link http://www.rescuework.jp
- */?><!DOCTYPE html>
+ */
+
+\Nos\Nos::main_controller()->addJavascript('static/apps/c5block/js/jquery.min.js');
+\Nos\Nos::main_controller()->addJavascript('static/apps/c5block/js/SimpleJSXslt.js');
+\Nos\Nos::main_controller()->addJavascript('static/apps/c5block/js/myjquery.js');
+?><!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8" />
